9feb0ee891c00a901bb7b1f7c6e19af4ef52044b
Arp2003 <136185997+Arp2003@users.noreply.github.com>
2026-05-07 21:46:29 +0530

    Update settings.py

--- pgmaster-backend/config/settings.py
+++ pgmaster-backend/config/settings.py
@@ -94,13 +94,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
